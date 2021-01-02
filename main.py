e87e4ba41156bb50a143c98b292199817468ef58
MarcelAdamski <marceladamskicf@gmail.com>
2021-01-02 02:01:08 +0000

    update Bot v1.3

--- main.py
+++ main.py
@@ -6,98 +6,35 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
 
-def TranslateNumbers(cookies, price):
+def TranslateNumbers(cookies, price, numbers_notation):
     c = cookies.replace(',','')
     c = c.split(" ")
+    exp = 3
     prefix = c[1]
-    if prefix[0] =="c":
+
+    if prefix[0] == "c":
         cookies = c[0]
-    elif c[1] == "million":
-        cookies = float(c[0]) * pow(10,6)
-    elif c[1] == "billion":
-        cookies = float(c[0]) * pow(10,9)
-    elif c[1] == "trillion":
-        cookies = float(c[0]) * pow(10,12)
-    elif c[1] == "quadrillion":
-        cookies = float(c[0]) * pow(10,15)
-    elif c[1] == "quintillion":
-        cookies = float(c[0]) * pow(10,18)
-    elif c[1] == "sextillion":
-        cookies = float(c[0]) * pow(10,21)
-    elif c[1] == "septillion":
-        cookies = float(c[0]) * pow(10,24)
-    elif c[1] == "octillion":
-        cookies = float(c[0]) * pow(10,27)
-    elif c[1] == "nonillion":
-        cookies = float(c[0]) * pow(10,30)
-    elif c[1] == "decillion":
-        cookies = float(c[0]) * pow(10,33)
-    elif c[1] == "undecillion":
-        cookies = float(c[0]) * pow(10,36)
-    elif c[1] == "duodecillion":
-        cookies = float(c[0]) * pow(10,39)
-    elif c[1] == "tredecillion":
-        cookies = float(c[0]) * pow(10,42)
-    elif c[1] == "quattuordecillion":
-        cookies = float(c[0]) * pow(10,45)
-    elif c[1] == "quindecillion":
-        cookies = float(c[0]) * pow(10,48)
-    elif c[1] == "sexdecillion":
-        cookies = float(c[0]) * pow(10,51)
-    elif c[1] == "septendecillion":
-        cookies = float(c[0]) * pow(10,54)
-    elif c[1] == "octodecillion":
-        cookies = float(c[0]) * pow(10,57)
-    elif c[1] == "novemdecillion":
-        cookies = float(c[0]) * pow(10,60)
-    elif c[1] == "vigintillion":
-        cookies = float(c[0]) * pow(10,63)
+    else:
+        prefix = prefix.split("\n")
+        prefix = prefix[0]
+        for notation in numbers_notation:
+            exp += 3
+            if prefix == notation:
+                cookies = float(c[0]) * pow(10, exp)
+                break
 
     p = price.replace(',', '')
     p = p.split(" ")
+    exp = 3
 
-    if len(p)==1:
+    if len(p) == 1:
         price = p[0]
-    elif p[1] == "million":
-        price = float(c[0]) * pow(10, 6)
-    elif p[1] == "billion":
-        price = float(c[0]) * pow(10, 9)
-    elif p[1] == "trillion":
-        price = float(c[0]) * pow(10, 12)
-    elif p[1] == "quadrillion":
-        price = float(c[0]) * pow(10, 15)
-    elif p[1] == "quintillion":
-        price = float(c[0]) * pow(10, 18)
-    elif p[1] == "sextillion":
-        price = float(c[0]) * pow(10, 21)
-    elif p[1] == "septillion":
-        price = float(c[0]) * pow(10, 24)
-    elif p[1] == "octillion":
-        price = float(c[0]) * pow(10, 27)
-    elif p[1] == "nonillion":
-        price = float(c[0]) * pow(10, 30)
-    elif p[1] == "decillion":
-        price = float(c[0]) * pow(10, 33)
-    elif p[1] == "undecillion":
-        price = float(c[0]) * pow(10, 36)
-    elif p[1] == "duodecillion":
-        price = float(c[0]) * pow(10, 39)
-    elif p[1] == "tredecillion":
-        price = float(c[0]) * pow(10, 42)
-    elif p[1] == "quattuordecillion":
-        price = float(c[0]) * pow(10, 45)
-    elif p[1] == "quindecillion":
-        price = float(c[0]) * pow(10, 48)
-    elif p[1] == "sexdecillion":
-        price = float(c[0]) * pow(10, 51)
-    elif p[1] == "septendecillion":
-        price = float(c[0]) * pow(10, 54)
-    elif p[1] == "octodecillion":
-        price = float(c[0]) * pow(10, 57)
-    elif p[1] == "novemdecillion":
-        price = float(c[0]) * pow(10, 60)
-    elif p[1] == "vigintillion":
-        price = float(c[0]) * pow(10, 63)
+    else:
+        for notation in numbers_notation:
+            exp += 3
+            if p[1] == notation:
+                price = float(p[0]) * pow(10, exp)
+                break
     return cookies, price
 
 def SaveGame(driver):
@@ -114,7 +51,7 @@ def SaveGame(driver):
         resetPosition.perform()
         time.sleep(2)
 
-        action.move_by_offset(484, 305).click()  # might need to change these numbers depending on your browser, but generally should work
+        action.move_by_offset(484, 305).click()  #might need to change these numbers depending on your browser, but generally should work
         action.perform()
         time.sleep(2)
 
@@ -124,8 +61,8 @@ def SaveGame(driver):
 
         time.sleep(2)
         driver.find_element_by_xpath('//*[@id="promptOption0"]').click()
-        print('Game saved+!')
-    except Exception as e:
+        print('Game saved!')
+    except Exception:
         print('Game save failed. Script is gonna try save again later.')
     time.sleep(5)
     driver.find_element_by_xpath('//div[@class="close menuClose"]').click()
@@ -142,16 +79,19 @@ def LoadGame(driver):
     time.sleep(2)
     driver.find_element_by_xpath('//*[@id="promptOption0"]').click()
 
-    print('Game loaded !')
+    print('Game loaded!')
     time.sleep(2)
 
-def Shopping(driver, product_prices, number_of_cookies):
+def GoldenCookie(driver):
+    try:
+        driver.find_element_by_xpath('//*[@id="shimmers"]/div').click()
+    except:
+        pass
+
+def Shopping(driver, product_prices, number_of_cookies, numbers_notation):
     for item in product_prices:  # index of item in shop is on [0] and price is on [1]
-        print(item[0], item[1].text)
         while True:
-            #print(number_of_cookies.text)
-            cookies, price = TranslateNumbers(number_of_cookies.text, item[1].text)
-            print(float(cookies), float(price))
+            cookies, price = TranslateNumbers(number_of_cookies.text, item[1].text, numbers_notation)
             if float(cookies) >= float(price):
                 product = driver.find_element_by_xpath("//div[@id='product{}']".format(str(item[0])))
                 product.click()
@@ -170,13 +110,21 @@ def Upgrading(driver):
         print('Not enough funds for upgrade!')
 
 def CheckShop(driver, product_prices):
-    e = driver.find_element_by_xpath("//div[@class='product locked disabled toggledOff']").get_attribute('id')
-    product_prices.clear()
-    for index in range(int(e[-1]) - 1, -1, -1):
-        product_prices.append((index, driver.find_element_by_xpath("//span[@id='productPrice{}']".format(str(index)))))
+    try:
+        e = driver.find_element_by_xpath("//div[@class='product locked disabled toggledOff']").get_attribute('id')
+        product_prices.clear()
+        for index in range(int(e[-1]) - 1, -1, -1):
+            product_prices.append((index, driver.find_element_by_xpath("//span[@id='productPrice{}']".format(str(index)))))
+        return True
+    except:
+        product_prices.clear()
+        for index in range(17, -1, -1):
+            product_prices.append((index, driver.find_element_by_xpath("//span[@id='productPrice{}']".format(str(index)))))
+        return False
+
 
 def Game(loops, small_click_loop, shop, save):
-    PATH = 'C:\Program Files (x86)\chromedriver.exe'
+    PATH = 'C:\Program Files (x86)\chromedriver.exe' #put here path to your chromedriver.exe
     driver = webdriver.Chrome(PATH)
     driver.get("https://orteil.dashnet.org/cookieclicker/")
     print(driver.title)
@@ -185,9 +133,14 @@ def Game(loops, small_click_loop, shop, save):
     #Comment above line when you start new game.
     #If you start new game you have to also clear save.txt file.
     #If you want to load your own game, just paste your game save to "save.txt", save and run with uncommented line
-
+    # Should click on the golden cookie if it is on screen
     product_prices = []
+    numbers_notation = ["million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion",
+                        "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion",
+                        "quattuordecillion", "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion",
+                        "vigintillion"]
     shop_or_upgrade = True
+    check_shop = True
     big_cookie = driver.find_element_by_xpath("//div[@id='bigCookie']")
     number_of_cookies = driver.find_element_by_xpath("//div[@id='cookies']")
 
@@ -195,32 +148,33 @@ def Game(loops, small_click_loop, shop, save):
     for i in range(1, loops):
         for j in range(small_click_loop):
             big_cookie.click()
-        cookies = number_of_cookies.text
-        print(cookies)
-
+        GoldenCookie(driver)
+        #print(number_of_cookies.text)
         if i % shop == 0:
-            CheckShop(driver, product_prices)
+            if check_shop:
+                check_shop = CheckShop(driver, product_prices)
             if shop_or_upgrade:
-                Shopping(driver, product_prices, number_of_cookies)
+                Shopping(driver, product_prices, number_of_cookies, numbers_notation)
                 shop_or_upgrade = False
             else:
                 Upgrading(driver)
                 shop_or_upgrade = True
-        if i % save == 0:  # 200 is every 1:55m #500 is every 5:40
+        if i % save == 0:
             SaveGame(driver)
 
 ###STARTING PARAMETERS###
 #SMALL_CLICK_LOOP - determines how many clicks in one "small loop" default is 100 and I recommend leaving this option at 100
 #SHOP - #determines how many "small clicks" multiplied by this variable to perform, before trying to buy anything from shop or try to upgrade.
-#50 means shopping about every minute
+#25 means shopping every ~40sec
+#50 means shopping every ~1:15
 #SAVE - #determines how many "small clicks" multiplied by this variable to perform, before trying to save a game.
-#100 means about every 2min
-#LOOPS - one loop is
+#50 means saving about every 1:20min
+#100 means saving about every 2:30
+#LOOPS - one loop is "SMALL_CLICK_LOOP" clicks on bigCookie, checks for shop and save.
 #########################
 
 SMALL_CLICK_LOOP = 100
-SHOP = 25
-SAVE = 50
+SHOP = 50
+SAVE = 100
 LOOPS = 5000
 Game(LOOPS, SMALL_CLICK_LOOP, SHOP, SAVE)
-
